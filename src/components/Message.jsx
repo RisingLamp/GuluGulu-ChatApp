89467a8a4eeb1c8819ae35a1a2eab864fff16c49
RisingLamp <uday.patil.st@gmail.com>
2024-04-04 15:05:49 +0530

    1 Enable User Search Option 2 Added ChatContext 3 Enable Chatting

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
-const Message = () => {
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
+
   return (
-    <div className='message'>
+    <div
+      ref={ref}
+      className={`message ${message.senderId === currentUser.uid && "owner"}`}
+    >
       <div className="messageInfo">
-      <img src="https://i.pinimg.com/originals/16/73/49/167349fb497f247d4530aae296e8fbbe.jpg" alt="Kaori" />
-      <span>just Now</span>
+        <img
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
+          alt=""
+        />
+        <span>just now</span>
       </div>
-      <div className="messageInfo"></div>
-      <p>Hello</p>
-     
+      <div className="messageContent">
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="" />}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
